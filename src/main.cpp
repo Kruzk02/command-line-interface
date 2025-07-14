0719b9a0cf10aadaf0aee57c68c87bb9d6e75837
thienphuc <ntphuc5484@gmail.com>
2025-07-14 11:03:53 +0700

    add parseInput()

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <ostream>
+#include <regex>
 #include <string>
+
+void parseInput(std::string input) {
+  std::regex words_regex(R"([^ ="]+|"[^"]+"|=)");
+  for (auto i = std::sregex_iterator(input.begin(), input.end(), words_regex);
+       i != std::sregex_iterator(); i++) {
+    std::string token = (*i).str();
+    if (token[0] == '"') {
+      token = token.substr(1, token.length() - 2);
+    }
+    std::cout << token << std::endl;
+  }
+}
 
 int main(int argc, char *argv[]) {
   std::string name;
@@ -7,4 +21,5 @@ int main(int argc, char *argv[]) {
   std::cout << "Please, enter your full name: ";
   std::getline(std::cin, name);
   std::cout << "Hello, " << name << std::endl;
+  parseInput(name);
 }
