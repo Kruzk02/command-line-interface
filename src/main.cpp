f9a82667efe0c3e5efc21125ed134d8ebd458a59
thienphuc <ntphuc5484@gmail.com>
2025-07-16 09:33:29 +0700

    refactor: change return type of parseInput from map to vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <map>
 #include <ostream>
 #include <regex>
 #include <string>
 #include <vector>
 
-std::map<std::string, std::vector<std::string>> parseInput(std::string input) {
-  std::string command;
-  std::vector<std::string> arguments;
-
+std::vector<std::string> parseInput(std::string input) {
+  if (input.empty())
+    return {};
   std::vector<std::string> inputs;
 
   std::regex words_regex(R"([^ ="]+|"[^"]+"|=)");
@@ -22,25 +20,16 @@ std::map<std::string, std::vector<std::string>> parseInput(std::string input) {
     inputs.push_back(token);
   }
 
-  command = inputs[0];
-  for (auto i = inputs.begin() + 1; i != inputs.end(); i++) {
-    arguments.push_back(*i);
-  }
-
-  return {{command, arguments}};
+  return inputs;
 }
 
 int main(int argc, char *argv[]) {
   std::string input;
 
   std::getline(std::cin, input);
-  std::map<std::string, std::vector<std::string>> wd = parseInput(input);
+  std::vector<std::string> inputs = parseInput(input);
 
-  for (const auto &pair : wd) {
-    std::cout << pair.first << ": ";
-    for (auto val : pair.second) {
-      std::cout << val << " ";
-    }
-    std::cout << std::endl;
+  for (const auto command : inputs) {
+    std::cout << command << std::endl;
   }
 }
