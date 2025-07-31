6c8ee6aef3bdb93f8ab842d209bfd73b9b38afb9
thienphuc <ntphuc5484@gmail.com>
2025-07-31 14:37:08 +0700

    refactor: add commandMap for more readable

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <cstdlib>
 #include <filesystem>
+#include <functional>
 #include <iostream>
 #include <ostream>
 #include <regex>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "../include/HelpCommand.h"
 #include "../include/Invoker.h"
 #include "../include/ListCommand.h"
 #include "../include/cdCommand.h"
+
+std::unordered_map<std::string, std::function<Command *()>> commandMap;
 
 std::vector<std::string> parse_input(const std::string &input) {
   if (input.empty()) return {};
@@ -30,36 +34,33 @@ std::vector<std::string> parse_input(const std::string &input) {
   return inputs;
 }
 
-void handle_command(std::vector<std::string> inputs) {
+void handle_command(Invoker &invoker, std::vector<std::string> inputs) {
   CommandContext ctx;
   Command *cmd = nullptr;
-  Invoker *invoker = new Invoker();
-  if (inputs[0] == "cd") {
-    if (inputs.size() - 1 >= 2) {
-      std::cout << "cd: too many arguments " << std::endl;
+  if (commandMap.find(inputs[0]) != commandMap.end()) {
+    cmd = commandMap[inputs[0]]();
+
+    if (inputs.size() > 1) {
+      ctx.arguments.assign(inputs.begin() + 1, inputs.end());
     } else {
-      cmd = new cdCommand();
       ctx.arguments.push_back(inputs[1]);
     }
-  } else if (inputs[0] == "ls") {
-    cmd = new ListCommand;
-    ctx.arguments.push_back(inputs[1]);
-  } else if (inputs[0] == "exit") {
-    exit(0);
-  } else if (inputs[0] == "help") {
-    cmd = new HelpCommand();
-  } else {
-    std::cout << inputs[0] << ": command not found" << std::endl;
-  }
-  if (cmd) {
-    invoker->setCommand(cmd);
-    invoker->execute(ctx);
-    delete invoker;
+
+    invoker.setCommand(cmd);
+    invoker.execute(ctx);
     delete cmd;
+  } else {
+    std::cout << inputs[0] << ": command not found\n";
   }
 }
 
 int main(int argc, char *argv[]) {
+  Invoker invoker;
+
+  commandMap["cd"] = []() { return new cdCommand(); };
+  commandMap["ls"] = []() { return new ListCommand(); };
+  commandMap["help"] = []() { return new HelpCommand(); };
+
   while (true) {
     std::cout << std::filesystem::current_path().string() << "$ ";
     std::string input;
@@ -67,6 +68,6 @@ int main(int argc, char *argv[]) {
     std::getline(std::cin, input);
     std::vector<std::string> inputs = parse_input(input);
 
-    handle_command(inputs);
+    handle_command(invoker, inputs);
   }
 }
