63c156ae531fe7c7c85e39f1e645d8489e444e9c
thienphuc <ntphuc5484@gmail.com>
2025-08-03 14:11:03 +0700

    refactor: make more readable

--- src/cdCommand.cpp
+++ src/cdCommand.cpp
@@ -8,22 +8,23 @@
 #include "../include/CommandContext.h"
 
 void cdCommand::execute(CommandContext &ctx) {
-  if (!ctx.arguments.empty()) {
-    std::string folder = ctx.arguments.at(0);
-
-    if (std::filesystem::exists(folder)) {
-      std::string newPath = std::filesystem::absolute(folder);
-      std::filesystem::current_path(newPath);
-
-      ctx.currentDirectory = std::filesystem::current_path();
-    } else {
-      std::cout << "cd: " << folder << ": No such file or directory"
-                << std::endl;
-    }
-  } else {
+  if (ctx.arguments.empty()) {
     const char *homedir = std::getenv("HOME");
     if (homedir) {
       ctx.currentDirectory = homedir;
+      std::filesystem::current_path(homedir);
     }
+    return;
   }
+
+  std::string folder = ctx.arguments.at(0);
+
+  if (!std::filesystem::exists(folder)) {
+    std::cout << "cd: " << folder << ": No such file or directory" << std::endl;
+    return;
+  }
+  std::string newPath = std::filesystem::absolute(folder);
+  std::filesystem::current_path(newPath);
+
+  ctx.currentDirectory = std::filesystem::current_path();
 }
